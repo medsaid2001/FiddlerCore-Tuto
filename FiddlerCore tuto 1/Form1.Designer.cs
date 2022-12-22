2596986249034be4d384815ec23ee21ece8912a5
medsaid2001 <46731194+medsaid2001@users.noreply.github.com>
2022-12-22 00:17:12 +0000

    Add files via upload
    This is a simple fiddler core tutorial from https://seosniffer.com. You can view the video here: https://www.youtube.com/watch?v=XVI9OHGYOVc&lc=Ugzby2wS5U3VPHJEvWl4AaABAg.9jIeAJiKHo39jKMVyEC-cl This application was created with Visual Studio 2022.

---
+++ FiddlerCore tuto 1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace FiddlerCore_tuto_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.install = new System.Windows.Forms.Button();
+            this.remove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(796, 413);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(3, 6);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 1;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(106, 6);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 23);
+            this.stop.TabIndex = 2;
+            this.stop.Text = "stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // install
+            // 
+            this.install.Location = new System.Drawing.Point(606, 6);
+            this.install.Name = "install";
+            this.install.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.install.Size = new System.Drawing.Size(75, 23);
+            this.install.TabIndex = 3;
+            this.install.Text = "install";
+            this.install.UseVisualStyleBackColor = true;
+            this.install.Click += new System.EventHandler(this.install_Click);
+            // 
+            // remove
+            // 
+            this.remove.Location = new System.Drawing.Point(713, 6);
+            this.remove.Name = "remove";
+            this.remove.Size = new System.Drawing.Size(75, 23);
+            this.remove.TabIndex = 4;
+            this.remove.Text = "remove";
+            this.remove.UseVisualStyleBackColor = true;
+            this.remove.Click += new System.EventHandler(this.remove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.remove);
+            this.Controls.Add(this.install);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button install;
+        private System.Windows.Forms.Button remove;
+    }
+}
+
---
+++ FiddlerCore tuto 1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Fiddler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiddlerCore_tuto_1
+{
+    public partial class Form1 : Form
+    {
+        private string separator = "----------------------------------------";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void startfiddler()
+        {
+            if (!FiddlerApplication.IsStarted())
+            {
+                FiddlerApplication.AfterSessionComplete += FiddlerApplication_AfterSessionComplete;
+                FiddlerApplication.Startup(8888, true, true, true);
+            }
+            else
+            {
+                appentext("fiddler is already running");
+            }
+        }
+
+        private void FiddlerApplication_AfterSessionComplete(Session oSession)
+        {
+            if(oSession.RequestMethod == "CONNECT")
+            {
+                return;
+            }
+            if(oSession ==null || oSession.oRequest==null || oSession.oRequest.headers == null)
+            {
+                return;
+            }
+            string headers = oSession.oRequest.headers.ToString();
+            string body = Encoding.UTF8.GetString(oSession.RequestBody);
+            string firstline = oSession.RequestMethod + " " + oSession.fullUrl + " " + oSession.oRequest.headers.HTTPVersion;
+            int at =  headers.IndexOf("\r\n");
+            if (at < 0)
+                return;
+            string output = firstline+ Environment.NewLine + headers.Substring(at+1);
+            if (body != null)
+            {
+                output += body + Environment.NewLine;
+            }
+            output += separator + Environment.NewLine;
+            appentext(output);
+        }
+
+        private void stopfiddler()
+        {
+            if (!FiddlerApplication.IsStarted())
+            {
+            }
+            else
+            {
+                FiddlerApplication.Shutdown();
+            }
+        }
+        private void installcert()
+        {
+            if (!CertMaker.rootCertExists())
+            {
+                CertMaker.createRootCert();
+                CertMaker.trustRootCert();
+            }
+        }
+        private void removecert()
+        {
+            if (CertMaker.rootCertExists())
+            {
+                CertMaker.removeFiddlerGeneratedCerts();
+            }
+        }
+        private void appentext(string value)
+        {
+            if (InvokeRequired)
+            {
+                richTextBox1.Invoke(new Action<string>(appentext), new object[] { value });
+                return;
+            }
+            richTextBox1.AppendText(value);
+            richTextBox1.ScrollToCaret();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            stopfiddler();
+            if (FiddlerApplication.oProxy != null)
+            {
+                if (FiddlerApplication.oProxy.IsAttached)
+                {
+                    FiddlerApplication.oProxy.Detach();
+                }
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            startfiddler();
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            stopfiddler();
+        }
+
+        private void install_Click(object sender, EventArgs e)
+        {
+            installcert();
+        }
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            removecert();
+        }
+    }
+}
